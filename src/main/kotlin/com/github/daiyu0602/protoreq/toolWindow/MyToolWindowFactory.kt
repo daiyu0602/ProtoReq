ef00069133259e3c26e8968d8f29a92e89a4e1b4
GitHub Action <action@github.com>
2026-03-02 02:58:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/daiyu0602/protoreq/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/daiyu0602/protoreq/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.daiyu0602.protoreq.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.daiyu0602.protoreq.MyBundle
+import com.github.daiyu0602.protoreq.services.MyProjectService
 import javax.swing.JButton
 
 
